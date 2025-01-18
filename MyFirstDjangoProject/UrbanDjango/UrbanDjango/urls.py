1dfb97bdb277cbcb9cf60fe804c56737bc1d2b0a
SFIRegret19 <fspirin8@gmail.com>
2025-01-18 22:42:14 +0700

    Создал 2 маршрута представлений регистрации

--- MyFirstDjangoProject/UrbanDjango/UrbanDjango/urls.py
+++ MyFirstDjangoProject/UrbanDjango/UrbanDjango/urls.py
@@ -18,14 +18,17 @@ from django.contrib import admin
 from django.urls import path
 # from task2.views import view1, view2
 # from example1.views import index
-from task4.views import base, platform_page_index, catalog_page_index, cart_page_index
+# from task4.views import base, platform_page_index, catalog_page_index, cart_page_index
+from task5.views import sign_up_by_django, sign_up_by_html
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('platform/', platform_page_index),
-    path('platform/games/', catalog_page_index),
-    path('platform/cart/', cart_page_index),
-    path('base/', base)
+    path('', sign_up_by_html),
+    path('django_sign_up/', sign_up_by_django)
+    # path('platform/', platform_page_index),
+    # path('platform/games/', catalog_page_index),
+    # path('platform/cart/', cart_page_index),
+    # path('base/', base)
     # path('index/', TemplateView.as_view(template_name='index2.html'))
 ]
